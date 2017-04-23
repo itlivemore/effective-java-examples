31ec71e27511edb49d09a5d0c3d96d95290b8b8d
laigc <Lgc1292416422>
2017-04-23 12:36:27 +0800

    继承中父类构造方法不能调用可覆盖的方法

---
+++ src/main/java/pers/ej/chapter04/item17/Sub.java
@@ -0,0 +1,26 @@
+package item17;
+
+import java.util.Date;
+
+/**
+ * 父类的构造器在子类的构造器之前运行。
+ * 所以子类中覆盖版本的方法在子类初始化之前就执行了，所以第一次打印null
+ * Created by laigc on 2017/4/23.
+ */
+public final class Sub extends Super {
+    private final Date date;
+
+    Sub() {
+        date = new Date();
+    }
+
+    @Override
+    public void overrideMe() {
+        System.out.println(date);
+    }
+
+    public static void main(String[] args) {
+        Sub sub = new Sub();
+        sub.overrideMe();
+    }
+}
---
+++ src/main/java/pers/ej/chapter04/item17/Super.java
@@ -0,0 +1,15 @@
+package item17;
+
+/**
+ * 父类的构造器中使用了可被覆盖的方法.
+ * Created by laigc on 2017/4/23.
+ */
+public class Super {
+    public Super() {
+        overrideMe();
+    }
+
+    public void overrideMe() {
+
+    }
+}
