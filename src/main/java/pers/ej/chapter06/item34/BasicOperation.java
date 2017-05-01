259c28b32970ca5532ee214eb2749616ac5025a2
laigc <Lgc1292416422>
2017-05-01 11:02:55 +0800

    用接口模拟可伸缩的枚举

---
+++ src/main/java/pers/ej/chapter06/item34/BasicOperation.java
@@ -0,0 +1,40 @@
+package pers.ej.chapter06.item34;
+
+/**
+ * 加减乘除操作
+ * Created by laigc on 2017/5/1.
+ */
+public enum BasicOperation implements Operation {
+    PLUS("+") {
+        @Override
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    }, MINUS("-") {
+        @Override
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    }, TIMES("*") {
+        @Override
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    }, DIVIDE("/") {
+        @Override
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    private final String symbol;
+
+    BasicOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+}
